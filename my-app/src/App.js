876cda7c7e100ea1c111c2ff06d1761ee4580018
kellyy8 <kellyyu2886@gmail.com>
2023-12-28 13:32:11 -0800

    Created 4 empty pages for website and linked them via a navigation bar in App.js.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import Home from './pages/Home';
+import Background from './pages/Background';
+import Projects from './pages/Projects';
+import Hobbies from './pages/Hobbies';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav className="navbar">
+        {/* <div className="logoImageWrapper">
+          <Link to="/"> <img src={logo} alt="logo" className="logoImage" /> </Link>
+        </div> */}
+        
+        <div className="navbarMenu">
+          <NavLink to="/" className="navbarItem"> Home </NavLink>
+          <NavLink to="/background" className="navbarItem"> Background </NavLink>
+          <NavLink to="/projects" className="navbarItem"> Projects </NavLink>
+          <NavLink to="/hobbies" className="navbarItem"> Hobbies </NavLink>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/background" element={<Background/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/hobbies" element={<Hobbies/>} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
