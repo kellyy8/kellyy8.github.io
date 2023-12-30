f2792c8a0860f7526d35c7e71571a10f309e2040
kellyy8 <kellyyu2886@gmail.com>
2023-12-30 00:23:58 -0800

    Cleaned up typography style, removed unused code, and added social links to Home page.

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,15 +1,28 @@
 import '../App.css';
 import './Home.css';
+import figma from '../logos/figma.png'
+import linkedin from '../logos/linkedin.png'
+import github from '../logos/github.png'
+import { Link } from 'react-router-dom'
+
 // import { NavLink } from "react-router-dom";
 // import {StyleSheet, View, Text, Image} from "react-native";
 // import { Fragment } from 'react';
 
 function Home() {
   return (
-    <div className='intro'>
-      <div className='introName'> KELLY YU </div>
-      <div className='introIntro'> A programmer, designer, and dancer </div>
-      <div className='introIntro'> innovating for an impact. </div>
+    <div className='baseContainer'>
+      <div className='introContainer'>
+        <div className='introName'> KELLY YU </div>
+        <div className='introIntro'> A programmer, designer, and dancer </div>
+        <div className='introIntro'> innovating for an impact. </div>
+      </div>
+
+      <div className='linksContainer'>
+        {/* <Link to='/'> <img src={figma} className='linksImage'></img> </Link> */}
+        <Link to='https://github.com/kellyy8'> <img src={github} className='linksImage'></img> </Link>
+        <Link to='https://www.linkedin.com/in/kelly-yu-b46406169/'> <img src={linkedin} className='linksImage'></img> </Link>
+      </div>
     </div>
 );
 }
