68d0be9296d039967ad1996535c119001f6cd85f
kellyy8 <kellyyu2886@gmail.com>
2024-01-01 16:03:38 -0800

    Used Modal from React bootstrap to display more info for each project. Still need to fix styling issues and unique content display for each project (add elements to Project Data).

---
+++ my-app/src/pages/ProjectOverlay.js
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import "./ProjectOverlay.css";
+
+export function ProjectOverlay({ isOpen, onClose, children }) {
+  return (
+    <Fragment>
+      {isOpen && (
+        <div className="overlay">
+          <div className="overlay__background" onClick={onClose} />
+          <div className="overlay__container">
+            <div className="overlay__controls">
+              <button
+                className="overlay__close"
+                type="button"
+                onClick={onClose}
+              />
+            </div>
+            {children}
+          </div>
+        </div>
+      )}
+    </Fragment>
+  );
+}
+
+export default ProjectOverlay;
--- my-app/src/pages/Projects.js
+++ my-app/src/pages/Projects.js
@@ -4,13 +4,71 @@ import items from './ProjectData';
 import { useState } from 'react';
 import { Link } from "react-router-dom";
 
+// import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import './bootstrap/css/bootstrap-theme.css'
+// import './bootstrap/css/bootstrap-theme.min.css'
+// import './bootstrap/css/bootstrap.css'
+// import './bootstrap/css/bootstrap.min.css'
+// import './bootstrap/js/bootstrap.js'
+// import './bootstrap/js/bootstrap.min.js'
+// import './bootstrap/config.json'
+
+var num = 0;
+
+/* Vertically centered 'modal' containing project info. */
+function DisplayInfo(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {items[num].title}
+          <h6> Mobile Development </h6>
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body style={{display: 'flex', flexFlow: 'column',}}>
+        <h4> Overview </h4>
+        <h6> Swift </h6>
+        <div> A Swift iOS mobile app that uses infinite zoom art and storytelling to promote mental wellness and creativity. </div>
+        <br/>
+
+        <h4> Links </h4>
+        <Link to='https://www.figma.com/file/6D9uJZFatgM5yHqAYgQ7OL/Pocket-Studio?type=design&node-id=148-23&mode=design&t=EMyyr5Brgo3Yavb9-0' target='_blank' style={{textDecoration: 'none'}}> Figma </Link>
+        <Link to='https://github.com/kellyy8/Pocket-Studio' target='_blank' style={{textDecoration: 'none'}}> GitHub </Link>
+        <Link to='https://docs.google.com/presentation/d/1KGYhBn93eg5blTtsxOIVH9WtdJooLkxAKk_ey-bHgsk/edit?usp=sharing' target='_blank' style={{textDecoration: 'none'}}> Demo </Link>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Link onClick={props.onHide}>Close</Link>
+      </Modal.Footer>
+
+    </Modal>
+  );
+}
+
 function Display({displayItems}) {
+  const [modalShow, setModalShow] = useState(false);
+  // const [item, setItem] = useState(items[0]);
+
   return (
     <div className='cardContainer'>
       {/* Use each 'item' data to populate each project's image and description. */}
       {displayItems.map((card, i) =>{
         return <div className='projectCard'>
-            <img src={card.image} alt={card.image} className='projectImage'></img>
+            <Link onClick={() => {setModalShow(true); num=i;}}>
+              <img src={card.image} alt={card.image} className='projectImage'></img>
+            </Link>
+
+            <DisplayInfo show={modalShow} onHide={() => setModalShow(false)}/>
+
             <div className='projectDescription'>
                 <p> <b> {i+1}- </b> </p>
                 <div>
@@ -25,7 +83,7 @@ function Display({displayItems}) {
   )
 }
 
-function Button({allCategories, filter}) {
+function FilterButton({allCategories, filter}) {
   return (
     <div className='filterMenu'>
         {/* Create a button for each category that calls the filter function when the button is clicked. */}
@@ -45,18 +103,18 @@ function Projects() {
       setDisplayItem(items);
     }
     else{
-      /* for every 'item' in 'items', store in 'filteredItems' array if item's category matches 'category' */
+      /* For every 'item' in 'items', store in 'filteredItems' array if item's category matches 'category' */
       const filteredItems = items.filter(item => item.category === category);
       setDisplayItem(filteredItems)
     }
   }
-
+  
   return (
     <div className='Projects'>
       <h1> PROJECTS </h1>
-      <p> Here are some of my favorite projects I've worked on!</p>
+      <p> Here are some of my favorite projects I've worked on! </p>
 
-      <Button allCategories={allCategories} filter={filter}/>
+      <FilterButton allCategories={allCategories} filter={filter}/>
       <Display displayItems={displayItem}/>
 
     </div>
